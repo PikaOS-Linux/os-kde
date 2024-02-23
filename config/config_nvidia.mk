deff353be9383c6fd85853549f5b41a440bd335c
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-02-23 23:19:17 +0300

    Update config_nvidia.mk

--- config/config_nvidia.mk
+++ config/config_nvidia.mk
@@ -28,7 +28,8 @@ DISTRO_PKGS=\
 	iucode-tool \
 	setserial \
 	git \
-	kernel-pika  \
+	pika-first-setup-gtk4 \
+	kernel-pika \
 	open-vm-tools \
 	open-vm-tools-desktop \
 	f2fs-tools \
@@ -49,8 +50,7 @@ LIVE_PKGS=\
 	expect \
 	gparted \
 	pikainstall \
-	vanilla-installer \
-	pika-first-setup-user 
+	pika-installer-gtk4
 
 # Packages to remove from installed system (usually installed as Recommends)
 RM_PKGS=\
@@ -63,13 +63,13 @@ RM_PKGS=\
 	ubuntu-wallpapers \
 	unattended-upgrades \
 	xul-ext-ubufox \
-	tilix \
+ 	tilix \
 	gnome-shell \
 	nautilus \
 	gnome-sushi \
-	apport \
+ 	apport \
 	gnome-terminal \
-	rtkit
+ 	rtkit
 
 # Packages not installed, but that may need to be discovered by the installer
 MAIN_POOL=\
